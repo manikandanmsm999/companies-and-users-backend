5a976bdd642c5e31522b455162aaee15ecf10127
Manikandan <manikandanmsm999@gmail.com>
2022-12-23 19:46:17 +0530

    updated update user in user service

--- Controller/servicesUser.js
+++ Controller/servicesUser.js
@@ -127,8 +127,10 @@ exports.createUser=async(req,res,next)=>{
 exports.updateUser=async(req,res,next)=>{
     try{
         let userId=req.body.userId;
+        let user={};
         const userIdCount=await userModel.find({userId:userId});
         if(userIdCount.length>=1){
+            user.userId=userId;
             let firstName=req.body.firstName;
             let lastName=req.body.lastName;
             let email=req.body.email;
@@ -136,12 +138,7 @@ exports.updateUser=async(req,res,next)=>{
             let dob=req.body.dob;
             if(validator.nullAndUndefinedCheck(firstName)){
                 if(validator.validateName(firstName)){
-                    const update=await userModel.findOneAndUpdate({userId:userId},{firstName:firstName},{new:true});
-                    if(update==null){
-                        const err=new Error(`Unable to update user`);
-                        err.status=400;
-                        throw err;
-                    }
+                    user.firstName=firstName;
                 }
                 else{
                     const err=new Error('First Name should be of length between 1 till 20');
@@ -151,12 +148,7 @@ exports.updateUser=async(req,res,next)=>{
             }
             if(validator.nullAndUndefinedCheck(lastName)){
                 if(validator.validateName(lastName)){
-                    const update=await userModel.findOneAndUpdate({userId:userId},{lastName:lastName},{new:true});
-                    if(update==null){
-                        const err=new Error(`Unable to update user`);
-                        err.status=400;
-                        throw err;
-                    }
+                    user.lastName=lastName;
                 }
                 else{
                     const err=new Error('Last Name should be of length between 1 till 20');
@@ -168,12 +160,7 @@ exports.updateUser=async(req,res,next)=>{
                 if(validator.validateEmail(email)){
                     const emailDuplicateCheker=await userModel.find({email:email});
                     if(emailDuplicateCheker.length<=0){
-                        const update=await userModel.findOneAndUpdate({userId:userId},{email:email},{new:true});
-                        if(update==null){
-                            const err=new Error(`Unable to update user`);
-                            err.status=400;
-                            throw err;
-                        }
+                        user.email=email;
                     }
                     else{
                         const err=new Error('User already exists with this email id');
@@ -189,12 +176,7 @@ exports.updateUser=async(req,res,next)=>{
             }
             if(validator.nullAndUndefinedCheck(designation)){
                 if(validator.validateDesignation(designation)){
-                    const update=await userModel.findOneAndUpdate({userId:userId},{designation:designation},{new:true});
-                    if(update==null){
-                        const err=new Error(`Unable to update user`);
-                        err.status=400;
-                        throw err;
-                    }
+                    user.designation=designation;
                 }
                 else{
                     const err=new Error('Designation should be of length between 2 till 20');
@@ -205,18 +187,21 @@ exports.updateUser=async(req,res,next)=>{
             if(validator.nullAndUndefinedCheck(dob)){
                 dob=new Date(dob);
                 if(validator.validateDOB(dob)){
-                    const update=await userModel.findOneAndUpdate({userId:userId},{dob:dob},{new:true});
-                    if(update==null){
-                        const err=new Error(`Unable to update user`);
-                        err.status=400;
-                        throw err;
-                    }
+                    user.dob=dob;
                 }
                 else{
                     const err=new Error('Date of birth should be less than todays current date');
                     err.status=401;
                     throw err;
                 }
+            }
+            console.log(user);
+            const update=await userModel.findOneAndUpdate({userId:userId},user,{new:true});
+            console.log(update);
+            if(update==null){
+                const err=new Error(`Unable to update user`);
+                err.status=400;
+                throw err;
             }
             res.status(200).json({"message":"User updated successfully"});
         }
